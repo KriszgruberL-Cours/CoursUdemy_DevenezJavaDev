65491d5c5f678f6c66a534bfae592511c109831b
Kiwah <l.kriszgruber@gmail.com>
2023-07-03 20:36:40 +0200

    Initial commit

---
+++ src/main/java/org/example/BureauDeChange.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class BureauDeChange {
+    public static void main(String[] args) {
+        int euro;
+        float dollars;
+        float results = convert(1500);
+        System.out.print(results);
+    }
+
+    public static float convert(int toConvert) {
+        return toConvert * 1.22f;
+    }
+}
